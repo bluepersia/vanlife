ff50b48cbc5b90945c4ed4d7917a29c6cd5efa3f
Marco Enrique <fuzzyslash@hotmail.com>
2026-04-06 18:26:42 -0600

    utilities.css setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import "./App.css";
 
 function App() {
   return (
-    <>
+    <div className="container">
       <h1>Hello World</h1>
       <p>This is lorem ipsum text</p>
-    </>
+    </div>
   );
 }
 
