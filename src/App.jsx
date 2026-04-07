03e7fddb31847f1fa3f983aa3334e142206a777f
Marco Enrique <fuzzyslash@hotmail.com>
2026-04-06 20:17:32 -0600

    Tanstack react query setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
 import AppLayout from "./components/AppLayout/AppLayout";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import "./server.js";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AppLayout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AppLayout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
