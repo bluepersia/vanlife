1c7993a74aedf001895b9bbb26e50a95d4b7f9a6
Marco Enrique <fuzzyslash@hotmail.com>
2026-06-19 13:31:01 -0600

    Setup React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import type { JSX } from "react/jsx-runtime";
 import "./App.css";
 import "./css/utilities.css";
+import { BrowserRouter, Routes } from "react-router";
 
 function App(): JSX.Element {
   const message = "Hello World";
   return (
-    <>
+    <BrowserRouter>
       <h1>{message}</h1>
-    </>
+      <Routes></Routes>
+    </BrowserRouter>
   );
 }
 
