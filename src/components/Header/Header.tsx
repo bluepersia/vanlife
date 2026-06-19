d0c390a4972b6d81ddfbb61158811165add8e598
Marco Enrique <fuzzyslash@hotmail.com>
2026-06-19 14:08:58 -0600

    Add app layo ut

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router";
+import type { JSX } from "react/jsx-runtime";
+import Nav from "../Nav/Nav";
+import styles from "./Header.module.css";
+import clsx from "clsx";
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={styles.header}>
+      <div className={clsx(styles["header-inner"], "container")}>
+        <Link to="/" className={styles.logo}>
+          #Vanlife
+        </Link>
+        <Nav
+          links={[
+            {
+              label: "About",
+              to: "about ",
+            },
+            {
+              label: "Vans",
+              to: "vans",
+            },
+          ]}
+        />
+      </div>
+    </header>
+  );
+}
